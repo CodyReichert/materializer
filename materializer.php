a8097d7759040eedd0ed3d8f49e1aac8718e4c6f
Cody Reichert <cody@reichertbrothers.com>
2015-07-05 03:50:42 -0500

    add shortcode for m_card

--- materializer.php
+++ materializer.php
@@ -26,7 +26,7 @@ class MaterializerBase {
         wp_register_style( 'M-Style-Bundle',
             plugins_url('scripts/dist/Materializer-bundle.css', __FILE__)
         );
-        wp_enqueue_style('M-Style-bundle');
+        wp_enqueue_style('M-Style-Bundle');
 
     }
 
@@ -40,3 +40,36 @@ class MaterializerBase {
     }
 
 }
+
+
+class MaterializerShortcodes {
+
+    public function basicCard( $atts ) {
+        $title = $atts['title'];
+        ob_start();
+        ?>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title"><?php echo $title; ?></span>
+                        <p>
+                            I am a very simple card. I am good at
+                            containing small bits of information. I
+                            am convenient because I require little
+                            markup to use effectively.
+                        </p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+}
+
+add_shortcode('m_card', array('MaterializerShortcodes', 'basicCard'));
