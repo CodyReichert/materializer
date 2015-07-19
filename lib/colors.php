7fbcee846a9ba69ef5b54f5d36d2cebe1249959e
Cody Reichert <cody@reichertbrothers.com>
2015-07-19 14:30:43 -0500

    add remaining color pallete to variables / add shadows file

--- lib/colors.php
+++ lib/colors.php
@@ -5,9 +5,7 @@
  */ 
 
 
-/**
- * RED
- */
+/** Red */
 $_red_lighten_5 = "#ffebee";
 $_red_lighten_4 = "#ffcdd2";
 $_red_lighten_3 = "#ef9a9a";
@@ -24,10 +22,7 @@ $_red_accent_3  = "#ff1744";
 $_red_accent_4  = "#d50000";
 
 
-/**
- * RED
- */
-
+/** Pink */
 $_pink_lighten_5 = "#fce4ec";
 $_pink_lighten_4 = "#f8bbd0";
 $_pink_lighten_3 = "#f48fb1";
@@ -44,9 +39,7 @@ $_pink_accent_3  = "#f50057";
 $_pink_accent_4  = "#c51162";
 
 
-/**
- * Purlple
- */
+/** Purple */
 $_purple_lighten_5 = "#f3e5f5";
 $_purple_lighten_4 = "#e1bee7";
 $_purple_lighten_3 = "#ce93d8";
@@ -63,9 +56,7 @@ $_purple_accent_3  = "#d500f9";
 $_purple_accent_4  = "#aa00ff";
 
 
-/** 
- * Deep Purple
- */
+/** Deep Purple */
 $_deep_purple_lighten_5 = "#ede7f6";
 $_deep_purple_lighten_4 = "#d1c4e9";
 $_deep_purple_lighten_3 = "#b39ddb";
@@ -82,9 +73,7 @@ $_deep_purple_accent_3  = "#651fff";
 $_deep_purple_accent_4  = "#6200ea";
 
 
-/**
- * Indigo
- */
+/** Indigo */
 $_indigo_lighten_5 = "#e8eaf6";
 $_indigo_lighten_4 = "#c5cae9";
 $_indigo_lighten_3 = "#9fa8da";
@@ -101,9 +90,7 @@ $_indigo_accent_3  = "#3d5afe";
 $_indigo_accent_4  = "#304ffe";
 
 
-/**
- * Blue
- */
+/** Blue */
 $_blue_lighten_5 = "#e3f2fd";
 $_blue_lighten_4 = "#bbdefb";
 $_blue_lighten_3 = "#90caf9";
@@ -119,21 +106,217 @@ $_blue_accent_2  = "#448aff";
 $_blue_accent_3  = "#2979ff";
 $_blue_accent_4  = "#2962ff";
 
-/**
- * Light Blue
- */
 
+/** Light Blue */
 $_light_blue_lighten_5 = "#e1f5fe";
-#b3e5fc light-blue lighten-4
-#81d4fa light-blue lighten-3
-#4fc3f7 light-blue lighten-2
-#29b6f6 light-blue lighten-1
-#03a9f4 light-blue
-#039be5 light-blue darken-1
-#0288d1 light-blue darken-2
-#0277bd light-blue darken-3
-#01579b light-blue darken-4
-#80d8ff light-blue accent-1
-#40c4ff light-blue accent-2
-#00b0ff light-blue accent-3
-#0091ea light-blue accent-4 
+$_light_blue_lighten_4 = "#b3e5fc";
+$_light_blue_lighten_3 = "#81d4fa";
+$_light_blue_lighten_2 = "#4fc3f7";
+$_light_blue_lighten_1 = "#29b6f6";
+$_light_blue           = "#03a9f4";
+$_light_blue_darken_1  = "#039be5";
+$_light_blue_darken_2  = "#0288d1";
+$_light_blue_darken_3  = "#0277bd";
+$_light_blue_darken_4  = "#01579b";
+$_light_blue_accent_1  = "#80d8ff";
+$_light_blue_accent_2  = "#40c4ff";
+$_light_blue_accent_3  = "#00b0ff";
+$_light_blue_accent_4  = "#0091ea";
+
+
+/** Cyan */
+$_cyan_lighten_5 = "#e0f7fa";
+$_cyan_lighten_4 = "#b2ebf2";
+$_cyan_lighten_3 = "#80deea";
+$_cyan_lighten_2 = "#4dd0e1";
+$_cyan_lighten_1 = "#26c6da";
+$_cyan           = "#00bcd4";
+$_cyan_darken_1  = "#00acc1";
+$_cyan_darken_2  = "#0097a7";
+$_cyan_darken_3  = "#00838f";
+$_cyan_darken_4  = "#006064";
+$_cyan_accent_1  = "#84ffff";
+$_cyan_accent_2  = "#18ffff";
+$_cyan_accent_3  = "#00e5ff";
+$_cyan_accent_4  = "#00b8d4";
+
+
+/** Teal */
+$_teal_lighten_5 = "#e0f2f1";
+$_teal_lighten_4 = "#b2dfdb";
+$_teal_lighten_3 = "#80cbc4";
+$_teal_lighten_2 = "#4db6ac";
+$_teal_lighten_1 = "#26a69a";
+$_teal           = "#009688";
+$_teal_darken_1  = "#00897b";
+$_teal_darken_2  = "#00796b";
+$_teal_darken_3  = "#00695c";
+$_teal_darken_4  = "#004d40";
+$_teal_accent_1  = "#a7ffeb";
+$_teal_accent_2  = "#64ffda";
+$_teal_accent_3  = "#1de9b6";
+$_teal_accent_4  = "#00bfa5";
+
+
+/** Green */
+$_green_lighten_5 = "#e8f5e9";
+$_green_lighten_4 = "#c8e6c9";
+$_green_lighten_3 = "#a5d6a7";
+$_green_lighten_2 = "#81c784";
+$_green_lighten_1 = "#66bb6a";
+$_green           = "#4caf50";
+$_green_darken_1  = "#43a047";
+$_green_darken_2  = "#388e3c";
+$_green_darken_3  = "#2e7d32";
+$_green_darken_4  = "#1b5e20";
+$_green_accent_1  = "#b9f6ca";
+$_green_accent_2  = "#69f0ae";
+$_green_accent_3  = "#00e676";
+$_green_accent_4  = "#00c853";
+
+
+/** Light Green */
+$_light_green_lighten_5 = "#f1f8e9";
+$_light_green_lighten_4 = "#dcedc8";
+$_light_green_lighten_3 = "#c5e1a5";
+$_light_green_lighten_2 = "#aed581";
+$_light_green_lighten_1 = "#9ccc65";
+$_light_green           = "#8bc34a";
+$_light_green_darken_1  = "#7cb342";
+$_light_green_darken_2  = "#689f38";
+$_light_green_darken_3  = "#558b2f";
+$_light_green_darken_4  = "#33691e";
+$_light_green_accent_1  = "#ccff90";
+$_light_green_accent_2  = "#b2ff59";
+$_light_green_accent_3  = "#76ff03";
+$_light_green_accent_4  = "#64dd17";
+
+
+/** Lime */
+$_lime_lighten_5 = "#f9fbe7";
+$_lime_lighten_4 = "#f0f4c3";
+$_lime_lighten_3 = "#e6ee9c";
+$_lime_lighten_2 = "#dce775";
+$_lime_lighten_1 = "#d4e157";
+$_lime           = "#cddc39";
+$_lime_darken_1  = "#c0ca33";
+$_lime_darken_2  = "#afb42b";
+$_lime_darken_3  = "#9e9d24";
+$_lime_darken_4  = "#827717";
+$_lime_accent_1  = "#f4ff81";
+$_lime_accent_2  = "#eeff41";
+$_lime_accent_3  = "#c6ff00";
+$_lime_accent_4  = "#aeea00";
+
+
+/** Yellow */
+$_yellow_lighten_5 = "#fffde7";
+$_yellow_lighten_4 = "#fff9c4";
+$_yellow_lighten_3 = "#fff59d";
+$_yellow_lighten_2 = "#fff176";
+$_yellow_lighten_1 = "#ffee58";
+$_yellow           = "#ffeb3b";
+$_yellow_darken_1  = "#fdd835";
+$_yellow_darken_2  = "#fbc02d";
+$_yellow_darken_3  = "#f9a825";
+$_yellow_darken_4  = "#f57f17";
+$_yellow_accent_1  = "#ffff8d";
+$_yellow_accent_2  = "#ffff00";
+$_yellow_accent_3  = "#ffea00";
+$_yellow_accent_4  = "#ffd600";
+
+
+/** Amber */
+$_amber_lighten_5 = "#fff8e1";
+$_amber_lighten_4 = "#ffecb3";
+$_amber_lighten_3 = "#ffe082";
+$_amber_lighten_2 = "#ffd54f";
+$_amber_lighten_1 = "#ffca28";
+$_amber           = "#ffc107";
+$_amber_darken_1  = "#ffb300";
+$_amber_darken_2  = "#ffa000";
+$_amber_darken_3  = "#ff8f00";
+$_amber_darken_4  = "#ff6f00";
+$_amber_accent_1  = "#ffe57f";
+$_amber_accent_2  = "#ffd740";
+$_amber_accent_3  = "#ffc400";
+$_amber_accent_4  = "#ffab00";
+
+
+/** Orange */
+$_orange_lighten_5 = "#fff3e0";
+$_orange_lighten_4 = "#ffe0b2";
+$_orange_lighten_3 = "#ffcc80";
+$_orange_lighten_2 = "#ffb74d";
+$_orange_lighten_1 = "#ffa726";
+$_orange           = "#ff9800";
+$_orange_darken_1  = "#fb8c00";
+$_orange_darken_2  = "#f57c00";
+$_orange_darken_3  = "#ef6c00";
+$_orange_darken_4  = "#e65100";
+$_orange_accent_1  = "#ffd180";
+$_orange_accent_2  = "#ffab40";
+$_orange_accent_3  = "#ff9100";
+$_orange_accent_4  = "#ff6d00";
+
+
+/** Deep Orange */
+$_deep_orange_lighten_5 = "#fbe9e7";
+$_deep_orange_lighten_4 = "#ffccbc";
+$_deep_orange_lighten_3 = "#ffab91";
+$_deep_orange_lighten_2 = "#ff8a65";
+$_deep_orange_lighten_1 = "#ff7043";
+$_deep_orange           = "#ff5722";
+$_deep_orange_darken_1  = "#f4511e";
+$_deep_orange_darken_2  = "#e64a19";
+$_deep_orange_darken_3  = "#d84315";
+$_deep_orange_darken_4  = "#bf360c";
+$_deep_orange_accent_1  = "#ff9e80";
+$_deep_orange_accent_2  = "#ff6e40";
+$_deep_orange_accent_3  = "#ff3d00";
+$_deep_orange_accent_4  = "#dd2c00";
+
+
+/** Brown */
+$_brown_lighten_5 = "#efebe9";
+$_brown_lighten_4 = "#d7ccc8";
+$_brown_lighten_3 = "#bcaaa4";
+$_brown_lighten_2 = "#a1887f";
+$_brown_lighten_1 = "#8d6e63";
+$_brown           = "#795548";
+$_brown_darken_1  = "#6d4c41";
+$_brown_darken_2  = "#5d4037";
+$_brown_darken_3  = "#4e342e";
+$_brown_darken_4  = "#3e2723";
+
+
+/** Grey */
+$_grey_lighten_5 = "#fafafa";
+$_grey_lighten_4 = "#f5f5f5";
+$_grey_lighten_3 = "#eeeeee";
+$_grey_lighten_2 = "#e0e0e0";
+$_grey_lighten_1 = "#bdbdbd";
+$_grey           = "#9e9e9e";
+$_grey_darken_1  = "#757575";
+$_grey_darken_2  = "#616161";
+$_grey_darken_3  = "#424242";
+$_grey_darken_4  = "#212121";
+
+
+/** Blue-Grey */
+$_blue_grey_lighten_5 = "#eceff1";
+$_blue_grey_lighten_4 = "#cfd8dc";
+$_blue_grey_lighten_3 = "#b0bec5";
+$_blue_grey_lighten_2 = "#90a4ae";
+$_blue_grey_lighten_1 = "#78909c";
+$_blue_grey           = "#607d8b";
+$_blue_grey_darken_1  = "#546e7a";
+$_blue_grey_darken_2  = "#455a64";
+$_blue_grey_darken_3  = "#37474f";
+$_blue_grey_darken_4  = "#263238";
+
+
+/** Flat */
+$black = "#000000";
+$white = "#000000";
+$transparent = "transparent";
---
+++ lib/shadows.php
@@ -0,0 +1,11 @@
+/*
+ * Materializer WP - shadows.php
+ * Copyright 2015 (c) - Cody Reichert
+ */
+
+
+/** Code Starts Here **/
+
+class Shadows {
+
+}
